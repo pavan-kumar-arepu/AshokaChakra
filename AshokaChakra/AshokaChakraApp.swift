0f51b43ad81d69aacd5a3666fde64383398471e3
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2023-06-23 22:31:48 +0200

    Initial Commit

---
+++ AshokaChakra/AshokaChakraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AshokaChakraApp.swift
+//  AshokaChakra
+//
+//  Created by Pavankumar Arepu on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AshokaChakraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
